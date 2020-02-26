68db1f84f00fa8e70fe5f7d639873ae30c532687
Aniket Chauhan <aniket6401@gmail.com>
2020-02-26 13:14:44 +0200

    Initial Implementation Done

--- Task2/main.py
+++ Task2/main.py
@@ -1,1 +1,19 @@
+arr = [10,65,2,75,150]
 
+# for c in range[0,5]:
+#   arr.append(input("enter number ")+ str(c+1) +": "))
+
+newarr=[]
+for x in range(0,len(arr)):
+    if int(arr[x]) >= 10 and int(arr[x]) <= 100:
+        newarr.append(arr[x])
+
+solutionsarr = []
+def opeartions (newarr):
+    solutionsarr.append(sum(newarr))
+    solutionsarr.append(min(newarr))
+    solutionsarr.append(max(newarr))
+    solutionsarr.append(sum(newarr)/len(newarr))
+    print(solutionsarr)
+
+opeartions(newarr)
