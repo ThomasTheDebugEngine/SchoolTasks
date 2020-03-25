11f15a9363d8e44e0bb70c69495e2982d91aa23d
ThomasTheDebugEngine <thomaskilinc09@gmail.com>
2020-03-25 08:00:02 +0200

    cleaned up variable definitions

--- Task5/main.py
+++ Task5/main.py
@@ -1,1 +1,13 @@
+input_str = input("input main string: ")
+pos_arr = input("input pos array: ")
+neg_arr = input("input negative array: ")
 
+pos_score = 0
+for pos in pos_arr:
+    pos_score += input_str.count(pos)
+
+neg_score = 0
+for neg in neg_arr:
+    neg_score += input_str.count(neg)
+
+print(pos_score - neg_score)
