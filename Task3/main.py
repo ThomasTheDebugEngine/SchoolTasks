99873f63819dd27c48387771176f3c84ba85cfbe
almightYev <yevkemail@gmail.com>
2020-03-29 00:15:23 +0300

    Improvement and help is needed :(

--- Task3/main.py
+++ Task3/main.py
@@ -1,1 +1,18 @@
+import sys
 
+#This part is getting the Inputs from the user.
+word = input("Please input a random letters: ")
+wordCount = len(word)
+number = int(input("Please input only int number: "))
+
+#This part check if the entered string can be equally divided into the entered number, if not it quits the program.(couldn't find Restart command)
+if (int(number) > 0) and (wordCount % number == 0):
+    print("Inputs were received and returned Okay.")
+else:
+    print("I am sorry but your inputs are invalid, Please try again :)")
+    sys.exit()
+
+# needs Improvement but it kinda works.
+result = "".join(sorted(set(word), key=word.index))
+out = [(result[i:i+number]) for i in range(0, len(result), number)]
+print(out)
