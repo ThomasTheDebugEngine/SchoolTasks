4b00aa6ddbfc0ee4dfcf0ed37a4293172b55873e
ThomasTheDebugEngine <thomaskilinc09@gmail.com>
2020-04-02 14:07:55 +0300

    added a main function to better control program flow

--- Task3/main.py
+++ Task3/main.py
@@ -1,18 +1,21 @@
 import sys
 
-#This part is getting the Inputs from the user.
-word = input("Please input a random letters: ")
-wordCount = len(word)
-number = int(input("Please input only int number: "))
+def program():
+    #This part is getting the Inputs from the user.
+    word = input("Please input a random letters: ")
+    wordCount = len(word)
+    number = int(input("Please input only int number: "))
 
-#This part check if the entered string can be equally divided into the entered number, if not it quits the program.(couldn't find Restart command)
-if (int(number) > 0) and (wordCount % number == 0):
-    print("Inputs were received and returned Okay.")
-else:
-    print("I am sorry but your inputs are invalid, Please try again :)")
-    sys.exit()
+    #This part check if the entered string can be equally divided into the entered number, if not it quits the program.(couldn't find Restart command)
+    if (int(number) > 0) and (wordCount % number == 0):
+        print("Inputs were received and returned Okay.")
+    else:
+        print("I am sorry but your inputs are invalid, Please try again :)")
+        program()
 
-# needs Improvement but it kinda works.
-result = "".join(sorted(set(word), key=word.index))
-out = [(result[i:i+number]) for i in range(0, len(result), number)]
-print(out)
+    # needs Improvement but it kinda works.
+    result = "".join(sorted(set(word), key=word.index))
+    out = [(result[i:i+number]) for i in range(0, len(result), number)]
+    print(out)
+
+program()
