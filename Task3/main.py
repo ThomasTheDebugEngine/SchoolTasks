aa9b337dd67f4a209dd20b639badf7af79eb19f1
almightYev <yevkemail@gmail.com>
2020-04-02 18:53:48 +0300

    Improved version, Duplication still and issue, working on it

--- Task3/main.py
+++ Task3/main.py
@@ -1,21 +1,19 @@
-import sys
+import textwrap
+from itertools import groupby
 
 def program():
     #This part is getting the Inputs from the user.
     word = input("Please input a random letters: ")
-    wordCount = len(word)
+    wordcount = len(word)
     number = int(input("Please input only int number: "))
-
-    #This part check if the entered string can be equally divided into the entered number, if not it quits the program.(couldn't find Restart command)
-    if (int(number) > 0) and (wordCount % number == 0):
+    #This part is used to check if the 2nd input is divided by the 1st part equally, if not, it repeats.
+    if (number > 0) and (int(wordcount) % number == 0):
         print("Inputs were received and returned Okay.")
     else:
         print("I am sorry but your inputs are invalid, Please try again :)")
         program()
-
-    # needs Improvement but it kinda works.
-    result = "".join(sorted(set(word), key=word.index))
-    out = [(result[i:i+number]) for i in range(0, len(result), number)]
-    print(out)
+    #This part divides the string into equal groups
+    string = (textwrap.wrap(word, number))
+    print(string)
 
 program()
