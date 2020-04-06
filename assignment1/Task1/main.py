a4dba82f0740a1da1716c9a228e062a00c407e64
ThomasTheDebugEngine <thomaskilinc09@gmail.com>
2020-04-06 13:50:37 +0300

    moved assignment 1 task1 to main.py

--- assignment1/Task1/main.py
+++ assignment1/Task1/main.py
@@ -1,1 +1,37 @@
+def splitN(word):
+    return list(word)
 
+word = input("Enter a word or a sentence: ")
+
+def splitOptionA(checkA):
+    return list(checkA)
+
+checkA = input("Enter letters / numbers: ")
+
+def splitOptionB(checkB):
+    return list(checkB)
+
+checkB = input("Enter different letters / numbers: ")
+
+print(splitN(word))
+print(splitOptionA(checkA))
+print(splitOptionB(checkA))
+
+countA = 0
+countB = 0
+for x in word:
+    for y in checkA:
+        if x == y:
+            countA = countA + 1
+
+
+for x in word:
+    for y in checkB:
+        if x == y:
+            countB = countB + 1
+
+print(countA)
+print(countB)
+
+sum = countA - countB
+print(sum)
