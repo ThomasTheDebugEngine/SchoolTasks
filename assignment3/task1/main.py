b69fa8543a6d0bb98df0fa72dde7f9117fa6f7f3
Aniket Chauhan <43927184+nickapic@users.noreply.github.com>
2020-05-29 21:44:43 +0300

    Two taks done

--- assignment3/task1/main.py
+++ assignment3/task1/main.py
@@ -25,7 +25,6 @@ class Database:
     def curs(self, value):  # setter for cursor property
         self._curs = value
 
-
     def connect(self, db_address):  # connect to database
         print("connecting to db...")
 
@@ -38,7 +37,6 @@ class Database:
 
         return connection
 
-
     def create_shop_table(self):  # create the article table
         print("creating shop table...")
 
@@ -50,7 +48,6 @@ class Database:
                 items TEXT
             )'''
         )
-
 
     def create_item_table(self):  # create the comment table
         print("creating Item table...")
@@ -71,7 +68,6 @@ class Database:
             )'''
         )
 
-
     def create_component_table(self):  # create the comment table
         print("creating coponents table...")
 
@@ -89,15 +85,14 @@ class Database:
             )'''
         )
 
-
     def check_shop_table_exists(self):
-        self.curs.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='shop' ")
+        self.curs.execute(
+            "SELECT count(name) FROM sqlite_master WHERE type='table' AND name='shop' ")
 
         if self.curs.fetchone()[0] == 1:
             return True  # table exists
         else:
             return False  # table does not exist
-
 
     def print_all(self):
         self.curs.execute("SELECT * from shop")
@@ -110,7 +105,6 @@ class Database:
         for row in self.curs:
             print(row)
 
-
     def check_item_table_exists(self):
         self.curs.execute(
             '''SELECT count(name) FROM sqlite_master WHERE type='table' AND name='items' ''')
@@ -118,7 +112,6 @@ class Database:
             return True  # table exists
         else:
             return False  # table does not exist
-
 
     def check_component_table_exists(self):
         self.curs.execute(
@@ -129,28 +122,23 @@ class Database:
         else:
             return False  # table does not exist
 
-
     def create_shop(self, id, name, address, item_id):
-        self.curs.execute('''INSERT INTO shop VALUES (?, ?, ?, ?)''', (id, name, address, item_id))
+        self.curs.execute(
+            '''INSERT INTO shop VALUES (?, ?, ?, ?)''', (id, name, address, item_id))
         self.conn.commit()
-        
-
 
     def create_item(self, id, barcode, name, description, unit_price, shop_id, timestamp):
         self.curs.execute('''INSERT INTO items VALUES (?, ?, ?, ?, ?, ?,?)''',
                           (id, barcode, name, description, unit_price, timestamp, shop_id))
         self.conn.commit()
 
-
     def create_component(self, id, name, quanity, timestamp, item_id):
         self.curs.execute('''INSERT INTO components VALUES (?, ?, ?, ?, ?)''',
                           (id, name, quanity, timestamp, item_id))
         self.conn.commit()
 
-
     def database_main(self):  # main function for the database
         self.start_database()  # start the database
-
 
     def start_database(self):  # start the database
         if not self.check_shop_table_exists():
@@ -168,27 +156,40 @@ class Database:
         else:
             print("components table already exists !")
 
-
     def modify_component_by_id(self, id, new_value):
-        self.curs.execute("UPDATE components SET quantity={0} WHERE component_id={1}".format(str(new_value), id,))
+        self.curs.execute("UPDATE components SET quantity={0} WHERE component_id={1}".format(
+            str(new_value), id,))
         self.conn.commit()
-
 
     def delete_component_by_id(self, id):
-        self.curs.execute("DELETE FROM components WHERE component_id={0}".format(id,))
+        self.curs.execute(
+            "DELETE FROM components WHERE component_id={0}".format(id,))
         self.conn.commit()
 
+    def consists_word(self, name):
+        self.curs.execute(
+            '''SELECT * FROM items WHERE name LIKE '%?%''', (name))
+        self.conn.commit()
+
+    def quanity_components(self, id):
+        self.curs.execute(
+            '''SELECT * FROM components WHERE item_id = '%?%''', (id))
+        print(self.curs.len)
+
     def get_all_relative_components(self, first_item, second_item):
-        self.curs.execute("SELECT * FROM items WHERE item_id IN (?, ?)",(first_item, second_item,))
+        self.curs.execute(
+            "SELECT * FROM items WHERE item_id IN (?, ?)", (first_item, second_item,))
         items_arr = self.curs.fetchall()  # get the searched items from db
-        
+
         item1_id = items_arr[0][0]  # extract the id from the items
         item2_id = items_arr[1][0]  # extract the id from the items
 
-        self.curs.execute("SELECT * FROM components WHERE item_id={0}".format(item1_id))
+        self.curs.execute(
+            "SELECT * FROM components WHERE item_id={0}".format(item1_id))
         item1_component_arr = self.curs.fetchall()
 
-        self.curs.execute("SELECT * FROM components WHERE item_id={0}".format(item2_id))
+        self.curs.execute(
+            "SELECT * FROM components WHERE item_id={0}".format(item2_id))
         item2_component_arr = self.curs.fetchall()
 
         commons_array = []
@@ -196,7 +197,7 @@ class Database:
             for item2 in item2_component_arr:
                 if(item1[1] == item2[1]):
                     commons_array.append(item1)
-        
+
         return commons_array
 
 
@@ -206,9 +207,11 @@ db.database_main()  # start the database
 db.create_shop(1, "IKI", "IKI Street 1", 5)
 db.create_shop(2, "MAXIMA", "Kaunas, Maksima Street 2", 6)
 db.create_item(3, 112233112233, "Zemaiciu bread", "", 1.55, time.time(), 1)
-db.create_item(4, 33333222111, "Klaipeda Milk", "Milk from Klaipeda", 2.69, time.time(), 1)
+db.create_item(4, 33333222111, "Klaipeda Milk",
+               "Milk from Klaipeda", 2.69, time.time(), 1)
 db.create_item(12, 99898989898, "Aukstaiciu Bread", "", 1.65, time.time(), 2)
-db.create_item(13, 99919191991, "Vilnius Milk", "Milk from Vinius", 2.99, time.time(), 2)
+db.create_item(13, 99919191991, "Vilnius Milk",
+               "Milk from Vinius", 2.99, time.time(), 2)
 
 db.create_component(5, "Flour", 1.50, time.time(), 3)
 db.create_component(6, "Water", 1.00, time.time(), 3)
